06324c839b239b1ce92c05f720c2b866a2c21542
sajidkhan2067 <sajidkhan382067@gmail.com>
2025-02-23 23:52:21 -0600

    Updated run.sh file

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e
+set -e  # Exit immediately if a command exits with a non-zero status.
 
 # Ensure correct ownership and permissions for /vol/web
 echo "Setting permissions for /vol/web..."
@@ -8,11 +8,12 @@ mkdir -p /vol/web/media /vol/web/static
 chown -R django-user:django-user /vol/web
 chmod -R 775 /vol/web
 
-# Run migrations and start the server
+# Run migrations and collect static files
 echo "Running migrations..."
 python manage.py wait_for_db
 python manage.py migrate
 python manage.py collectstatic --noinput
 
+# Start Django application and keep the container running
 echo "Starting application..."
-exec "$@"
+exec gunicorn --bind 0.0.0.0:8000 app.wsgi:application --workers=4 --threads=2 --timeout=120
