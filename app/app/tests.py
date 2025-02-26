5078957465da78e8b465ecab8acfef33e1fc3de9
sajidkhan2067 <sajidkhan382067@gmail.com>
2025-02-25 18:31:29 -0600

    Linting errors are removed

--- app/app/tests.py
+++ app/app/tests.py
@@ -9,10 +9,9 @@ from app import calc
 class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         self.assertEqual(calc.add(3, 8), 11)
-      
+     
     def test_subtract_numbers(self):
         self.assertEqual(calc.subtract(5, 11), 6)
 
     # def test_subtract_numbers_negative(self):
     #     self.assertEqual(calc.subtract(11, 15), -6)
-
